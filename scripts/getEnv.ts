f8b9ec993222c09af255bf783378ea041b973b51
Alien Deployer <m@a17.name>
2024-04-08 19:26:29 +0300

    setup env, ci; add, update packages

--- scripts/getEnv.ts
+++ scripts/getEnv.ts
@@ -24,6 +24,7 @@ export function getEnv() {
       },
       privateKey: {
         type: 'string',
+        default: 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', // default account 0
       },
     }).argv;
 }
